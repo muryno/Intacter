8b295373a7012eb924cc92bb146c9707eb36906f
Muraino Yakubu <murainoy@yahoo.com>
2020-05-24 14:50:13 +0100

    nav changes

---
+++ Intacter/controller/ContainerVC.swift
@@ -0,0 +1,21 @@
+//
+//  ContainerVC.swift
+//  Intacter
+//
+//  Created by Muraino Yakubu on 5/23/20.
+//  Copyright © 2020 Muraino Yakubu. All rights reserved.
+//
+
+import UIKit
+
+class ContainerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ Intacter/controller/menu/MenuVC.swift
@@ -0,0 +1,15 @@
+//
+//  MenuVC.swift
+//  Intacter
+//
+//  Created by Muraino Yakubu on 5/24/20.
+//  Copyright © 2020 Muraino Yakubu. All rights reserved.
+//
+
+import UIKit
+
+class MenuVC: UIViewController {
+
+
+    
+}
